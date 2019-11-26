3dc492270a3d13b9d67deb8eff727357a0b4f749
JayGupta13 <jaygupta928@gmail.com>
2019-11-26 13:08:33 +0530

    Worked on review comment of UI Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { SearchItemPipe } from './pipe/search-item.pipe';
 import {MatInputModule} from '@angular/material/input';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -23,7 +23,7 @@ export class FilterComponent implements OnInit {
   updatedMinMaxValue() {
     this.productInPriceRange = []
     let minValue = +(<HTMLInputElement> document.getElementById('priceMin')).value; // getting min and max value from template file
-    let maxValue = +(<HTMLInputElement>document.getElementById('priceMax')).value;    
+    let maxValue = +(<HTMLInputElement>document.getElementById('priceMax')).value;
     for(let i=0; i<this.storeProduct.length; i++) {
       if(this.storeProduct[i].price >= minValue  && this.storeProduct[i].price <=maxValue) { // product condition check btween min and max
        
